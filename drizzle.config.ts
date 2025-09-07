5868e4c58ca19d226d14a4743b1f4f75d0b9f2e6
Bryan Nelson <bnelson523@gmail.com>
2025-09-07 18:09:11 -0400

    chore: add db migrate script (#208)

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "drizzle-kit";
 
-const connectionString = process.env.TEST_DATABASE_URL ?? process.env.DATABASE_URL;
+const connectionString =
+  process.env.NODE_ENV === "test"
+    ? process.env.TEST_DATABASE_URL ?? process.env.DATABASE_URL
+    : process.env.DATABASE_URL;
 
 if (!connectionString) {
   throw new Error("DATABASE_URL, ensure the database is provisioned");
